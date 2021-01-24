477e1d651a4a7d40a74f375b003b7402a888cd83
gabriel-gpb <gabriel_pereira94@hotmail.com>
2021-01-24 15:56:38 -0300

    meu primeiro commit

---
+++ teste-multiplicacao/js/main.js
@@ -0,0 +1,20 @@
+//Multiplicação Simples
+let valor1 = parseInt(gets());
+let valor2 = parseInt(gets());
+let total = valor1 * valor2; // Altere o valor da variável com o cálculo esperado 
+console.log("PROD = " + total);
+
+//Visita na feira
+let line = gets().split(" ");
+let A = parseInt(line[0]);
+let B = parseInt(line[1]);
+let total = A + B; // Altere o valor da variável com o cálculo esperado
+console.log("X = " + total);
+
+//Folha de pagamento
+let numero_colaborador = parseInt(gets());
+let horas_trabalhadas = parseInt(gets());
+let valor_hora = parseFloat(gets());
+let salary = parseFloat(100 * 5.50).toFixed(2); // Digite aqui o calculo do salário
+console.log("NUMBER = " + numero_colaborador);
+console.log("SALARY = U$ " + salary);
